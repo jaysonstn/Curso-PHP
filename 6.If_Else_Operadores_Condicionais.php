ef5ba9fc8e63ffd5b8ae7efaff331d5e653e8b72
jaysonstn <jaysonstn@gmail.com>
2025-09-24 21:31:54 -0300

    Upload

---
+++ 6.If_Else_Operadores_Condicionais.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cusro PHP</title>
+</head>
+
+<body>
+
+    <?php
+    if (false) {
+        echo 'Verdadeiro';
+    } else {
+        echo 'Falso';
+    }
+
+    ?>
+
+    <!-----------------|Operador == |------------------->
+
+    <?php
+    if (2 == 2) {
+        echo 'Verdadeiro';
+    } else {
+        echo 'Falso';
+    }
+
+    ?>
+
+    <!-----------------|Operador === |------------------->
+
+    <?php
+    if (2 === 2) {
+        echo 'Verdadeiro';
+    } else {
+        echo 'Falso';
+    }
+
+    ?>
+
+    <!-----------------|Operador != ou <> |-------------->
+
+    <?php
+    if (2 !=  2) {
+        echo 'Verdadeiro';
+    } else {
+        echo 'Falso';
+    }
+    ?>
+
+    <!--------------|Operador !== |---------------------->
+
+    <?php
+    if (2 !== 3) {
+        echo 'Verdadeiro';
+    } else {
+        echo 'Falso';
+    }
+
+    ?>
+
+    
+
+
+</body>
+
+</html>
